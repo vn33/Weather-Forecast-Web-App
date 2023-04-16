f5fcf090548fa553e2f67733bdefc5cc1f93eb48
vn33 <vishalpnaik666@gmail.com>
2023-04-16 18:47:39 +0530

    added backend functionality, added images for sky dynamically

--- backend.py
+++ backend.py
@@ -3,17 +3,13 @@ import requests
 API_KEY = "da69afd01925e5fca2697ab4810d5619"
 
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
